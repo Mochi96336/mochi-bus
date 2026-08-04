b8324a4ce994e330e80678e042cc894b715cced9
Mochi96336 <144137376+Mochi96336@users.noreply.github.com>
2026-08-05 03:29:02 +0800

    fix map return entry trust

--- web/map/entry-bootstrap.ts
+++ web/map/entry-bootstrap.ts
@@ -1,5 +1,7 @@
+import { discardUntrustedReturnHomeEntry } from '../navigation/return-home-entry'
 import { installReturnHomeNavigation } from '../navigation/return-home'
 import { installInitialMapViewGate } from './initial-view-gate'
 
+discardUntrustedReturnHomeEntry()
 installReturnHomeNavigation('.map-home[href="/"]')
 installInitialMapViewGate()
