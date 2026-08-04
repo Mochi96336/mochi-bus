1baef7f7aacc6b670b0d9f688209b00b42fdf95d
Mochi96336 <144137376+Mochi96336@users.noreply.github.com>
2026-08-05 03:29:07 +0800

    fix setup return entry trust

--- web/setup/entry-bootstrap.ts
+++ web/setup/entry-bootstrap.ts
@@ -1,3 +1,5 @@
+import { discardUntrustedReturnHomeEntry } from '../navigation/return-home-entry'
 import { installReturnHomeNavigation } from '../navigation/return-home'
 
+discardUntrustedReturnHomeEntry()
 installReturnHomeNavigation('.brand[href="/"], .topbar .icon-link[href="/"]')
